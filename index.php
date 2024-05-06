156c7deb5d77c40ae88c06ad85a029bc38feaa95
pietrocruciata <cruciata9748@gmail.com>
2024-05-06 14:15:17 +0200

    add: bootstrap cdn

--- index.php
+++ index.php
@@ -47,22 +47,37 @@ for ($i = 0; $i < count($hotels); $i++) {
     $parking = $hotel['parking'];
     $vote = $hotel['vote'];
     $distance_to_center = $hotel['distance_to_center'];
-    
-    ?>
-    <div>
-    <?php echo $name?>
-    </div>
-    <div>
-    <?php echo $description?>
-    </div>
-    <div>
-    <?php echo $parking?>
-    </div>
-    <div>
-    <?php echo $vote?>
-    </div>
-    <div>
-    <?php echo $distance_to_center?>
-    </div>
-    <?php
+
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <title>Hotel</title>
+    </head>
+
+    <body>
+        <div>
+            <?php echo $name ?>
+        </div>
+        <div>
+            <?php echo $description ?>
+        </div>
+        <div>
+            <?php echo $parking ?>
+        </div>
+        <div>
+            <?php echo $vote ?>
+        </div>
+        <div>
+            <?php echo $distance_to_center ?>
+        </div>
+    </body>
+
+    </html>
+
+<?php
 }
